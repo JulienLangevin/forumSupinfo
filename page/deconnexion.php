cd89c3bd4b1cd5513fdaa1144c8059890587b076
JulienLangevin <julien_langevin@ymail.com>
2016-10-05 14:14:29 +0200

    Update deconnexion.php

--- page/deconnexion.php
+++ page/deconnexion.php
@@ -1,8 +1,10 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+session_start();
+session_destroy();
+unset($_COOKIE['id']);
+unset($_COOKIE['username']);
+unset($_COOKIE['password']);
+
+header ('Location: connexion.php');
 
